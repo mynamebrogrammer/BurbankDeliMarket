ce6c8ee5bc86d4eb050878eb0e52c98167f219a4
Habib Maksoud <habibmaksoud@gmail.com>
2023-06-29 13:55:35 -0700

    added coming soon to menu page

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import comingSoon from '../../src/assets/comingsoon.jpg';
 
 function MenuPage() {
   return (
     <div>
       <h2>Menu</h2>
+      <p>We are currently in the process of building a mouth-watering menu...Comming soon!</p>
+      <img src={comingSoon} alt="Coming Soon" />
       
       {/* Add more Sandwich components or other menu items */}
     </div>
