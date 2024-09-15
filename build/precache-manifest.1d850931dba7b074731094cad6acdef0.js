6ac889cab88c724d0c0f3363dc0e66943497c34a
Habib Maksoud <habibmaksoud@gmail.com>
2024-09-15 15:37:45 -0700

    2nd try at updating menu

---
+++ build/precache-manifest.1d850931dba7b074731094cad6acdef0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc3eceefe7014e54f06ae3e8a84cf16b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3952e66418265588605c",
+    "url": "/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "5ba51bd83777c366f826",
+    "url": "/static/css/main.a157697c.chunk.css"
+  },
+  {
+    "revision": "3952e66418265588605c",
+    "url": "/static/js/2.444bebc2.chunk.js"
+  },
+  {
+    "revision": "9a6c3bcb7f69e70096783bb0f3d49144",
+    "url": "/static/js/2.444bebc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2de089a508c239c09c5",
+    "url": "/static/js/3.56f309cc.chunk.js"
+  },
+  {
+    "revision": "5ba51bd83777c366f826",
+    "url": "/static/js/main.9d09c965.chunk.js"
+  },
+  {
+    "revision": "38ec4cf2e01df461dc7e",
+    "url": "/static/js/runtime-main.4d10b052.js"
+  },
+  {
+    "revision": "642bf26844ea94afc72abac5a0b84814",
+    "url": "/static/media/burbankdelilogo.642bf268.png"
+  },
+  {
+    "revision": "2fdfc6b5a87bb682632beaea7ad8bd46",
+    "url": "/static/media/sandwich.2fdfc6b5.png"
+  },
+  {
+    "revision": "1eee606fe8d23da16d69f162d4a64448",
+    "url": "/static/media/sandwich2.1eee606f.png"
+  }
+]);
