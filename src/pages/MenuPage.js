6ed636cd7d7975a5408201bed356126f59f16991
Habib Maksoud <habibmaksoud@gmail.com>
2024-09-15 09:11:04 -0700

    added menu pages for visuals, removed coming soon

--- src/pages/MenuPage.js
+++ src/pages/MenuPage.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import comingSoon from '../../src/assets/comingsoon.jpg';
+import sandwich from '../assets/sandwich.png';
+import sandwich2 from '../assets/sandwich2.png';
+import '../menu.css';
 
 function MenuPage() {
   return (
     <div>
       <h2>Menu</h2>
-      <p>We are currently in the process of building a mouth-watering menu...Coming soon!</p>
-      <img src={comingSoon} alt="Coming Soon" />
+      <p>We also cater and take custom orders!</p>
+      <p>Call us at (818)846-6231 </p>
+      <div className="menu-container">
+        <div className="menu-item">
+          <img src={sandwich} alt="Sandwich" />
+          <h3>Delicious Sandwiches</h3>
+        </div>
+        
+        {/* Add more menu items here */}
+        <div className="menu-item">
+          <img src={sandwich2} alt="Sandwich" />
+          <h3>More Delicious Sandwiches</h3>
+          </div>
+      </div>
       
       {/* Add more Sandwich components or other menu items */}
     </div>
